41f83fcb6f789bec2fd8292b5c3159a951b3129a
Jerinislam05 <jerinislam05@gmail.com>
2023-11-07 23:49:20 +0000

    learned about getElementsByTagName

--- dom.js
+++ dom.js
@@ -1,1 +1,10 @@
-console.log('Running');
+console.log('hello world');
+/* DOM
+----------
+what can do javascript with dom
+1. javascript can change all the HTML elements, attributes in the page
+2. can change all the css styles in the page
+3. can remove existing HTML elements and attributes
+4. can add new HTML elements and attributes
+5. javascript can react to all existing HTML events in the page
+*/
