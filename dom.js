caef1d229fe2709066b3c63e0f7f7b82424011b8
Jerinislam05 <jerinislam05@gmail.com>
2023-11-10 21:34:24 +0000

    lear about querySelector

--- dom.js
+++ dom.js
@@ -1,4 +1,4 @@
-console.log('hello world');
+// console.log('hello world');
 /* DOM
 ----------
 what can do javascript with dom
@@ -8,3 +8,20 @@ what can do javascript with dom
 4. can add new HTML elements and attributes
 5. javascript can react to all existing HTML events in the page
 */
+const sections = document.querySelectorAll('section');
+// console.log(sections);
+for (const section of sections) {
+    // console.log(section);
+    section.style.border = '2px solid steelblue';
+    section.style.marginBottom = '5px';
+    section.style.borderRadius = '15px';
+    section.style.paddingLeft = '7px';
+    section.style.backgroundColor = 'lightGray';
+}
+
+// const placesContainer = document.getElementById('places-container');
+// placesContainer.style.backgroundColor = 'yellow';
+
+const placesContainer = document.getElementById('places-container');
+placesContainer.classList.add('text-center');
+placesContainer.classList.remove('large-text');
